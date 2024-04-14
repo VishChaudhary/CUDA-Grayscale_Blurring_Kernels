db203d4b470eca90ca3c6b7410d322ea1546402c
Vish Chaudhary <97370632+VishChaudhary@users.noreply.github.com>
2024-04-14 19:48:07 -0400

    Update and rename color_grayscale.cu to grayscale_blurring.cu

--- grayscale_blurring.cu
+++ grayscale_blurring.cu
@@ -1,6 +1,8 @@
 #include "stdio.h"
 #include "math.h"
 
+#define N 3
+#define BLUR_SIZE (N-1/2)
 #define channels 3
 
 __global__  void color2grayscale(unsigned char *Pin, unsigned char *Pout, int width, int height){
@@ -17,6 +19,29 @@ __global__  void color2grayscale(unsigned char *Pin, unsigned char *Pout, int wi
     unsigned char b = Pin[rgbOffset + 2];
 
     Pout[grayscaleOffset] = 0.21*r + 0.71*g + 0.07*b;
+  }
+}
+
+__global__ void blurKernel(unsigned char* Pin, unsigned char* Pout, int width, int height){
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+  if(col < width && row< height){
+    int pixelVal = 0;
+    int pixelCount = 0;
+
+    for(int blurRow = -BLUR_SIZE; blurRow < BLUR_SIZE + 1; ++blurRow){
+      for(int blurCol = -BLUR_SIZE; blurCol < BlUR_SIZE + 1; ++blurCol){
+          int currRow = row + blurRow;
+          int currCol = col + blurCol;
+
+          if(currRow>= 0 && currRow < height && currCol >= 0 && currCol < width){
+            pixelVal += Pin[currRow*width+currCol];
+            pixelCount++;
+          }
+      }
+    }
+    Pout[row*width+col] = (unsigned char) (pixelVal/pixelCount++;);
   }
 }
 
